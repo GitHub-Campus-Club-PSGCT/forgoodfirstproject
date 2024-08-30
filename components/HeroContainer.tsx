eb1600120940b2a2c85d315b70c41ad84b209770
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-08-30 19:16:40 +0530

    changed Landing Page

--- components/HeroContainer.tsx
+++ components/HeroContainer.tsx
@@ -4,11 +4,11 @@ export const HeroContainer = () => {
   return (
     <>
       <Hero className="hero" data-color-mode="dark">
-        <Hero.Heading size="display">Committing to a better&nbsp;future</Hero.Heading>
+        <Hero.Heading size="display">Discover or get&nbsp;Discovered</Hero.Heading>
         <Hero.Description size="300">
-          Lend your skills to an open source project focused on the <a href="https://digitalpublicgoods.net/digital-public-goods/" target="_blank" rel="noopener noreferrer">Digital Public Goods (DPGs)</a>. From fighting climate change, to solving world hunger, your efforts will contribute to creating a better future for everyone. Together, we can drive positive and lasting contributions to the world, one commit at a time.
+          Find interesting projects to work on this weekend or get found by talent across PSG College of Technology to collaborate on your next big thing
           <br /><br />
-          Explore a DPG repo below to get started.
+          Explore a repository or a submission below to get started!
         </Hero.Description>
       </Hero>
     </>
